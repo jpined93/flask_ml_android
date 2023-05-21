9307398bd002272d0255965f8d2f1f520876fa4d
jpined93 <jpined93@gmail.com>
2023-05-21 06:46:00 -0500

    adding per disease model

--- src/app.py
+++ src/app.py
@@ -15,19 +15,29 @@ from PIL import Image
 import numpy as np
 import base64
 from io import BytesIO
+from rembg import remove
 
 MODEL_PATH="/app/"
 # Load Model
 #==============================================================================
 
 Model_json = MODEL_PATH+"model.json"
-Model_weights = MODEL_PATH+"model.h5"
+#Model_weights = MODEL_PATH+"model.h5"
 
 model_json = open(Model_json, 'r')
 loaded_model_json = model_json.read()
 model_json.close()
-model = tf.keras.models.model_from_json(loaded_model_json)
-model.load_weights(Model_weights)
+# model = tf.keras.models.model_from_json(loaded_model_json)
+# model.load_weights(Model_weights)
+
+models_path=["model_fito.h5","model_mazorca_negra.h5","model_monoliosis_ef.h5","model_monoliosis_intermedia_sf.h5"]
+models=[]
+
+for path in models_path:
+    Model_weights = MODEL_PATH+path
+    tmp= tf.keras.models.model_from_json(loaded_model_json)
+    tmp.load_weights(Model_weights)
+    models.append(tmp)
 
 
 def decode_img(img):
@@ -36,6 +46,8 @@ def decode_img(img):
         im_file = BytesIO(im_bytes)  # convert image to file-like object
         img = Image.open(im_file)   # img is now PIL Image object
         img = img.resize((300, 300))
+        img=remove(img)
+
         print ("image decoded")
         return img 
     except Exception as e:
@@ -56,13 +68,25 @@ def preprocessing_img(img):
     
 def predict_img(x):
     try:
-        preds = model.predict(x)
-        preds=preds.tolist()[0]
-        if np.argmax(np.array(preds))==0:
-            class_pred="Cat"
-        elif np.argmax(np.array(preds))==1:
-            class_pred="Dog"
-        
+        preds=[]
+        for model in models:
+            individual_preds = model.predict(x)
+            individual_preds=individual_preds.tolist()[0]
+            preds.append(individual_preds[0])  
+
+        class_pred=4 if np.argmax(np.array(preds))<0.5 else np.argmax(np.array(preds))
+        class_prob=preds[class_pred] if class_pred!=4 else 1-preds[class_pred]
+        if class_pred==0:
+            class_pred=f"Fito {class_prob:.0%}"
+        elif class_pred==1:
+            class_pred=f"Mazorca negra {class_prob:.0%}"
+        elif class_pred==2:
+            class_pred=f"Monoliasis {class_prob:.0%}"
+        elif class_pred==3:
+            class_pred=f"Monoliasis Intermedia {class_prob:.0%}"
+        elif class_pred==4:
+            class_pred=f"Sano {class_prob:.0%}"
+
         print ("image predicted")
         return jsonify({"img":str(class_pred)})
     except Exception as e:
@@ -98,19 +122,76 @@ def val_img():
                 print(f"Exception preprocessing img: {e}" )
                 return jsonify({f"error":f"Exception preprocessing img: {e}"})
 
+            
+
             try:
-                preds = model.predict(x)
-                preds=preds.tolist()[0]
-                if np.argmax(np.array(preds))==0:
-                    class_pred="Cat"
-                elif np.argmax(np.array(preds))==1:
-                    class_pred="Dog"
+                preds=[]
+                for model in models:
+                    individual_preds = model.predict(x)
+                    individual_preds=individual_preds.tolist()[0]
+                    preds.append(individual_preds[0]) 
+
+                class_pred=np.argmax(np.array(preds))
+                class_prob=preds[class_pred]
                 
-                print ("image predicted")
-                return jsonify({"img":str(class_pred)})
+                if class_prob<0.5:
+                    class_pred="Sano"
+                    class_prob=1-class_prob
+                elif class_pred==0:
+                    class_pred="Fito"
+                elif class_pred==1:
+                    class_pred="Mazorca negra"
+                elif class_pred==2:
+                    class_pred="Monoliasis"
+                elif class_pred==3:
+                    class_pred="Monoliasis Intermedia"
+
+                message=f"Predicción: {class_pred} con probabilidad de {class_prob}"
+                return jsonify({"img":message})
             except Exception as e:
                 print(f"Exception making predictions img: {e}" )
-                return jsonify({f"error":f"Exception making predictions img: {e}"})
+                return None
+
+            # try:
+            #     preds=[]
+            #     for model in models:
+            #         individual_preds = model.predict(x)
+            #         individual_preds=individual_preds.tolist()[0]
+            #         preds.append(individual_preds[0])  
+
+            #     class_pred=4 if np.argmax(np.array(preds))<0.5 else np.argmax(np.array(preds))
+            #     class_prob=preds[class_pred] if class_pred!=4 else 1-np.argmax(np.array(preds))
+            #     if class_pred==0:
+            #         class_pred=f"Fito {class_prob:.0%}"
+            #     elif class_pred==1:
+            #         class_pred=f"Mazorca negra {class_prob:.0%}"
+            #     elif class_pred==2:
+            #         class_pred=f"Monoliasis {class_prob:.0%}"
+            #     elif class_pred==3:
+            #         class_pred=f"Monoliasis Intermedia {class_prob:.0%}"
+            #     elif class_pred==4:
+            #         class_pred=f"Sano {class_prob:.0%}"
+
+            #     print ("image predicted")
+            #     return jsonify({"img":str(class_pred)})
+            # except Exception as e:
+            #     print(f"Exception making predictions img: {e}" )
+            #     return None
+            
+            
+            # try:
+            #     preds = model.predict(x)
+            #     preds=preds.tolist()[0]
+            #     if np.argmax(np.array(preds))==0:
+            #         class_pred="Cat"
+            #     elif np.argmax(np.array(preds))==1:
+            #         class_pred="Dog"
+                
+            #     print ("image predicted")
+            #     return jsonify({"img":str(class_pred)})
+            # except Exception as e:
+            #     print(f"Exception making predictions img: {e}" )
+            #     return jsonify({f"error":f"Exception making predictions img: {e}"})
 
 
 
